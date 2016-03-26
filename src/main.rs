866632ccd2ad868be1ead7dec961958082c36b05
Thomas Castiglione <castiglione@mac.com>
2016-03-27 00:28:55 +0800

    a main loop, sort of

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
 extern crate sdl2;
+use std::thread;
+use std::time::Duration;
 
-fn create_window() -> Result<sdl2::video::Window, String> {
-    let ctx = try!(sdl2::init());
-    let video_ctx = try!(ctx.video());
-    let window = video_ctx.window("Scape", 640, 480).position_centered().opengl().build();
+struct Context {
+    sdl: sdl2::Sdl,
+    window: sdl2::video::Window,
+    events: sdl2::EventPump,
+}
+
+fn init() -> Result<Context, String> {
+    let sdl_ctx = try!(sdl2::init());
+    let video_ctx = try!(sdl_ctx.video());
+    let window = try!(video_ctx.window("Scape", 640, 480).position_centered().opengl().build().map_err(|e| e.to_string()));
+    let events = try!(sdl_ctx.event_pump());
     
-    return window.map_err(|e| e.to_string());
+    Ok(Context {sdl: sdl_ctx, window: window, events: events})
 }
 
 fn main() {
-    create_window().unwrap().show();
+    let mut context = init().unwrap();
+    context.window.show();
+    
+    let mut should_quit = false;
+    while !should_quit {
+        for event in context.events.poll_iter() {
+            should_quit = match event {
+                sdl2::event::Event::Quit {..} => true,
+                _ => false
+            };
+        }
+        
+        thread::sleep(Duration::from_millis(100));
+    }
 }
