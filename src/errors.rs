02bdd798a9f4f6c6afead7483dcf48cda2d00708
Thomas Castiglione <castiglione@mac.com>
2016-03-27 16:43:59 +0800

    put text on the screen

--- src/errors.rs
+++ src/errors.rs
@@ -7,14 +7,16 @@ use std::fmt;
 #[derive(Debug)]
 pub enum ScapeError {
     Str(String),
-    Inner(Box<error::Error>)
+    Inner(Box<error::Error>),
+    TextureValue(sdl2::render::TextureValueError)
 } 
 
 impl fmt::Display for ScapeError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             ScapeError::Str(ref err) => write!(f, "{}", err),
-            ScapeError::Inner(ref err) => write!(f, "{}", err)
+            ScapeError::Inner(ref err) => write!(f, "{}", err),
+            ScapeError::TextureValue(_) => write!(f, "TextureValueError")
         }
     }
 }
@@ -23,14 +25,16 @@ impl error::Error for ScapeError {
     fn description(&self) -> &str {
         match *self {
             ScapeError::Str(ref err) => err,
-            ScapeError::Inner(ref err) => err.description()
+            ScapeError::Inner(ref err) => err.description(),
+            ScapeError::TextureValue(_) => "TextureValueError"
         }
     }
 
     fn cause(&self) -> Option<&error::Error> {
         match *self {
             ScapeError::Str(_) => None,
-            ScapeError::Inner(ref err) => Some(err.as_ref())
+            ScapeError::Inner(ref err) => Some(err.as_ref()),
+            ScapeError::TextureValue(_) => None
         }
     }
 }
@@ -50,5 +54,17 @@ impl From<&'static str> for ScapeError {
 impl From<sdl2::video::WindowBuildError> for ScapeError {
     fn from(err: sdl2::video::WindowBuildError) -> ScapeError {
         ScapeError::Inner(Box::new(err))
+    }  
+}
+
+impl From<sdl2::IntegerOrSdlError> for ScapeError {
+    fn from(err: sdl2::IntegerOrSdlError) -> ScapeError {
+        ScapeError::Inner(Box::new(err))
+    }  
+}
+
+impl From<sdl2::render::TextureValueError> for ScapeError {
+    fn from(err: sdl2::render::TextureValueError) -> ScapeError {
+        ScapeError::TextureValue(err)
     }  
 }
--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,40 @@ mod errors;
 use std::thread;
 use std::time::Duration;
 use std::error::Error;
+use std::path::Path;
 
 use sdl2::messagebox;
 use sdl2::event::Event;
+use sdl2::pixels::Color;
+use sdl2::render::Renderer;
+use sdl2::render::TextureQuery;
+use sdl2::rect::Rect;
+use sdl2_ttf::Font;
 
 use errors::ScapeError;
 
+static SCREEN_WIDTH : u32 = 1280;
+static SCREEN_HEIGHT : u32 = 720;
+
 fn main() {
     if let Err(e) = _main() {
-        let _ = messagebox::show_simple_message_box(messagebox::MESSAGEBOX_ERROR, "Error", e.description(), Option::None); 
+        let _ = messagebox::show_simple_message_box(messagebox::MESSAGEBOX_ERROR, "Error", e.description(), None); 
     }
 }
 
 fn _main() -> Result<(), ScapeError> {
     let sdl_ctx = try!(sdl2::init());
     let video_ctx = try!(sdl_ctx.video());
-    let ttf_ctx = try!(sdl2_ttf::init().map_err(|e| e.to_string())); //because the actual error type is private
+    let ttf_ctx = try!(sdl2_ttf::init().map_err(|e| e.to_string())); //because the actual error type is private  
     
-    let mut window = try!(video_ctx.window("Scape", 1280, 720).position_centered().opengl().build());
+    let window = try!(video_ctx.window("Scape", SCREEN_WIDTH, SCREEN_HEIGHT).position_centered().opengl().build());
     let mut events = try!(sdl_ctx.event_pump());
+    let mut renderer = try!(window.renderer().build());
     
-    window.show();
+    let arial128 = try!(ttf_ctx.load_font(Path::new("C:\\Windows\\Fonts\\arial.ttf"), 128));
     
     let mut should_quit = false;
+    let mut i = 0;
     while !should_quit {
         for event in events.poll_iter() {
             should_quit = match event {
@@ -36,10 +47,11 @@ fn _main() -> Result<(), ScapeError> {
             };
         }
         
-        if let Err(e) = frame() {
-            let _ = messagebox::show_simple_message_box(messagebox::MESSAGEBOX_ERROR, "Error", e.description(), Option::Some(&window)); 
+        if let Err(e) = frame(&mut renderer, &arial128, i) {
+            let _ = messagebox::show_simple_message_box(messagebox::MESSAGEBOX_ERROR, "Error", e.description(), None); //should be Some but there's a library bug
             should_quit = true;
         }
+        i += 1;
         
         thread::sleep(Duration::from_millis(100));
     }
@@ -47,6 +59,19 @@ fn _main() -> Result<(), ScapeError> {
     Ok(())
 }
 
-fn frame() -> Result<(), ScapeError> {
-    Err("no game yet".into())
+fn frame(renderer: &mut Renderer, font: &Font, i: i32) -> Result<(), ScapeError> {
+    let surface = try!(font.render(format!("{}", i).as_str()).blended(Color::RGBA(255, 0, 0, 255)).map_err(|e| e.to_string())); //more private error types :(
+    let mut texture = try!(renderer.create_texture_from_surface(&surface));
+    let TextureQuery { width, height, .. } = texture.query();
+    let target = Rect::new((SCREEN_WIDTH / 2 - width / 2) as i32, 
+                           (SCREEN_HEIGHT / 2 - height / 2) as i32,
+                           width, 
+                           height);
+
+    renderer.set_draw_color(Color::RGBA(195, 217, 255, 255));
+    renderer.clear();
+    renderer.copy(&mut texture, None, Some(target));
+    renderer.present();
+    
+    Ok(())
 }
