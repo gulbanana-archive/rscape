d9038d9189f45139a235279d4d0c163e4a0e8645
Thomas Castiglione <castiglione@mac.com>
2016-03-27 14:41:01 +0800

    inner error handler

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,18 @@ use std::time::Duration;
 use sdl2::messagebox;
 use sdl2::messagebox::MessageBoxFlag;
 use sdl2::event::Event;
+use sdl2::video::Window;
+
+fn main() {
+    if let Err(e) = _main() {
+        let _ = messagebox::show_simple_message_box(messagebox::MESSAGEBOX_ERROR, "Error", &e, Option::None); 
+    }
+}
 
 fn _main() -> Result<(), String> {
     let sdl_ctx = try!(sdl2::init());
     let video_ctx = try!(sdl_ctx.video());
-    let mut window = try!(video_ctx.window("Scape", 640, 480).position_centered().opengl().build().map_err(|e| e.to_string()));
+    let mut window = try!(video_ctx.window("Scape", 1280, 720).position_centered().opengl().build().map_err(|e| e.to_string()));
     let mut events = try!(sdl_ctx.event_pump());
     
     window.show();
@@ -22,14 +29,17 @@ fn _main() -> Result<(), String> {
             };
         }
         
+        if let Err(e) = frame(&window) {
+            let _ = messagebox::show_simple_message_box(messagebox::MESSAGEBOX_ERROR, "Error", &e, Option::Some(&window)); 
+            should_quit = true;
+        }
+        
         thread::sleep(Duration::from_millis(100));
     }
     
     Ok(())
 }
 
-fn main() {
-    if let Err(e) = _main() {
-        let _ = messagebox::show_simple_message_box(MessageBoxFlag::empty(), "Error", &e, Option::None); 
-    }
+fn frame(window: &Window) -> Result<(), String> {
+    Err("no game yet".into())
 }
