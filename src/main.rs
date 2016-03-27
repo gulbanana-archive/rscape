59f15e0b66e2e54bcaf98e96aa271a1af930e9d0
Thomas Castiglione <castiglione@mac.com>
2016-03-27 17:00:30 +0800

    honestly i don't want to go THAT fast

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,11 @@ use std::thread;
 use std::time::Duration;
 use std::error::Error;
 use std::path::Path;
-use std::collections::HashMap;
 
 use sdl2::messagebox;
 use sdl2::event::Event;
 use sdl2::pixels::Color;
 use sdl2::render::Renderer;
-use sdl2::render::Texture;
 use sdl2::render::TextureQuery;
 use sdl2::rect::Rect;
 use sdl2_ttf::Font;
@@ -38,7 +36,6 @@ fn _main() -> Result<(), ScapeError> {
     let mut renderer = try!(window.renderer().build());
     
     let arial128 = try!(ttf_ctx.load_font(Path::new("C:\\Windows\\Fonts\\arial.ttf"), 128));
-    let mut glyph_cache: HashMap<i32, Texture> = HashMap::new();
     
     let mut should_quit = false;
     let mut i = 0;
@@ -50,29 +47,23 @@ fn _main() -> Result<(), ScapeError> {
             };
         }
         
-        if let Err(e) = frame(&mut renderer, &arial128, &mut glyph_cache, i) {
+        if let Err(e) = frame(&mut renderer, &arial128, i) {
             let _ = messagebox::show_simple_message_box(messagebox::MESSAGEBOX_ERROR, "Error", e.description(), None); //should be Some but there's a library bug
             should_quit = true;
         }
         i += 1;
-        if i > 9 {
-            i = 0;
-        }
+
         
-        //thread::sleep(Duration::from_millis(100));
+        thread::sleep(Duration::from_millis(10));
     }
     
     Ok(())
 }
 
-fn frame(renderer: &mut Renderer, font: &Font, cache: &mut HashMap<i32, Texture>, i: i32) -> Result<(), ScapeError> {
-    if !cache.contains_key(&i) {
-        let surface = try!(font.render(format!("{}", i).as_str()).blended(Color::RGBA(255, 0, 0, 255)).map_err(|e| e.to_string())); //more private error types :(
-        let new_texture = try!(renderer.create_texture_from_surface(&surface));
-        cache.insert(i, new_texture);
-    }
+fn frame(renderer: &mut Renderer, font: &Font, i: i32) -> Result<(), ScapeError> {
+    let surface = try!(font.render(format!("{}", i).as_str()).blended(Color::RGBA(255, 0, 0, 255)).map_err(|e| e.to_string())); //more private error types :(
+    let mut texture = try!(renderer.create_texture_from_surface(&surface));
     
-    let texture = cache.get_mut(&i).unwrap();
     let TextureQuery { width, height, .. } = texture.query();
     let target = Rect::new((SCREEN_WIDTH / 2 - width / 2) as i32, 
                            (SCREEN_HEIGHT / 2 - height / 2) as i32,
@@ -81,7 +72,7 @@ fn frame(renderer: &mut Renderer, font: &Font, cache: &mut HashMap<i32, Texture>
 
     renderer.set_draw_color(Color::RGBA(195, 217, 255, 255));
     renderer.clear();
-    renderer.copy(texture, None, Some(target));
+    renderer.copy(&mut texture, None, Some(target));
     renderer.present();
     
     Ok(())
