31a8e9cc44e1724d5aa3543a5cac40a0315c89b9
Thomas Castiglione <castiglione@mac.com>
2016-03-27 14:27:22 +0800

    add a top-level event handler

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,35 @@
 extern crate sdl2;
 use std::thread;
 use std::time::Duration;
+use sdl2::messagebox;
+use sdl2::messagebox::MessageBoxFlag;
+use sdl2::event::Event;
 
-struct Context {
-    sdl: sdl2::Sdl,
-    window: sdl2::video::Window,
-    events: sdl2::EventPump,
-}
-
-fn init() -> Result<Context, String> {
+fn _main() -> Result<(), String> {
     let sdl_ctx = try!(sdl2::init());
     let video_ctx = try!(sdl_ctx.video());
-    let window = try!(video_ctx.window("Scape", 640, 480).position_centered().opengl().build().map_err(|e| e.to_string()));
-    let events = try!(sdl_ctx.event_pump());
+    let mut window = try!(video_ctx.window("Scape", 640, 480).position_centered().opengl().build().map_err(|e| e.to_string()));
+    let mut events = try!(sdl_ctx.event_pump());
     
-    Ok(Context {sdl: sdl_ctx, window: window, events: events})
-}
-
-fn main() {
-    let mut context = init().unwrap();
-    context.window.show();
+    window.show();
     
     let mut should_quit = false;
     while !should_quit {
-        for event in context.events.poll_iter() {
+        for event in events.poll_iter() {
             should_quit = match event {
-                sdl2::event::Event::Quit {..} => true,
+                Event::Quit {..} => true,
                 _ => false
             };
         }
         
         thread::sleep(Duration::from_millis(100));
+    }
+    
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = _main() {
+        let _ = messagebox::show_simple_message_box(MessageBoxFlag::empty(), "Error", &e, Option::None); 
     }
 }
