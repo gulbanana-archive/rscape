99a0d9e5d376b6747275d0d204738722449724dc
Thomas Castiglione <castiglione@mac.com>
2016-03-28 18:11:32 +0800

    rustfmt

--- src/errors.rs
+++ src/errors.rs
@@ -8,15 +8,15 @@ use std::fmt;
 pub enum ScapeError {
     Str(String),
     Inner(Box<error::Error>),
-    TextureValue(sdl2::render::TextureValueError)
-} 
+    TextureValue(sdl2::render::TextureValueError),
+}
 
 impl fmt::Display for ScapeError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             ScapeError::Str(ref err) => write!(f, "{}", err),
             ScapeError::Inner(ref err) => write!(f, "{}", err),
-            ScapeError::TextureValue(_) => write!(f, "TextureValueError")
+            ScapeError::TextureValue(_) => write!(f, "TextureValueError"),
         }
     }
 }
@@ -26,7 +26,7 @@ impl error::Error for ScapeError {
         match *self {
             ScapeError::Str(ref err) => err,
             ScapeError::Inner(ref err) => err.description(),
-            ScapeError::TextureValue(_) => "TextureValueError"
+            ScapeError::TextureValue(_) => "TextureValueError",
         }
     }
 
@@ -34,7 +34,7 @@ impl error::Error for ScapeError {
         match *self {
             ScapeError::Str(_) => None,
             ScapeError::Inner(ref err) => Some(err.as_ref()),
-            ScapeError::TextureValue(_) => None
+            ScapeError::TextureValue(_) => None,
         }
     }
 }
@@ -54,17 +54,17 @@ impl From<&'static str> for ScapeError {
 impl From<sdl2::video::WindowBuildError> for ScapeError {
     fn from(err: sdl2::video::WindowBuildError) -> ScapeError {
         ScapeError::Inner(Box::new(err))
-    }  
+    }
 }
 
 impl From<sdl2::IntegerOrSdlError> for ScapeError {
     fn from(err: sdl2::IntegerOrSdlError) -> ScapeError {
         ScapeError::Inner(Box::new(err))
-    }  
+    }
 }
 
 impl From<sdl2::render::TextureValueError> for ScapeError {
     fn from(err: sdl2::render::TextureValueError) -> ScapeError {
         ScapeError::TextureValue(err)
-    }  
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -21,24 +21,31 @@ use errors::ScapeError;
 
 fn main() {
     if let Err(e) = _main() {
-        let _ = messagebox::show_simple_message_box(messagebox::MESSAGEBOX_ERROR, "Error", e.description(), None); 
+        let _ = messagebox::show_simple_message_box(messagebox::MESSAGEBOX_ERROR,
+                                                    "Error",
+                                                    e.description(),
+                                                    None);
     }
 }
 
 fn _main() -> Result<(), ScapeError> {
-    let mut screen_width : u32 = 1280;
-    let mut screen_height : u32 = 720;
-    
+    let mut screen_width: u32 = 1280;
+    let mut screen_height: u32 = 720;
+
     let sdl_ctx = try!(sdl2::init());
     let video_ctx = try!(sdl_ctx.video());
-    let ttf_ctx = try!(sdl2_ttf::init().map_err(|e| e.to_string())); //because the actual error type is private  
-    
-    let window = try!(video_ctx.window("Scape", screen_width, screen_height).position_centered().resizable().opengl().build());
+    let ttf_ctx = try!(sdl2_ttf::init().map_err(|e| e.to_string())); //because the actual error type is private
+
+    let window = try!(video_ctx.window("Scape", screen_width, screen_height)
+                               .position_centered()
+                               .resizable()
+                               .opengl()
+                               .build());
     let mut events = try!(sdl_ctx.event_pump());
     let mut renderer = try!(window.renderer().build());
-    
+
     let arial = try!(ttf_ctx.load_font(Path::new("C:\\Windows\\Fonts\\arial.ttf"), 64));
-    
+
     let mut should_quit = false;
     let mut i = 0;
     while !should_quit {
@@ -46,50 +53,56 @@ fn _main() -> Result<(), ScapeError> {
             match event {
                 Event::Quit { .. } => {
                     should_quit = true;
-                },
+                }
                 Event::Window { win_event_id: WindowEventId::Resized, data1, data2, .. } => {
                     screen_width = data1 as u32;
                     screen_height = data2 as u32;
-                },
-                _ => ()
+                }
+                _ => (),
             };
         }
-        
-        //prep the screen
+
+        // prep the screen
         renderer.set_draw_color(Color::RGBA(195, 217, 255, 255));
         renderer.clear();
-        
-        //draw a number to a texture
+
+        // draw a number to a texture
         let mut dot = try!(render_text(&mut renderer, &arial, i));
         let TextureQuery { width: glyph_width, height: glyph_height, .. } = dot.query();
-        
-        if (screen_width/glyph_width) * glyph_width < screen_width || (screen_height/glyph_height) * glyph_height < screen_height {
-            screen_width = (screen_width/glyph_width) * glyph_width;
-            screen_height = (screen_height/glyph_height) * glyph_height;
+
+        if (screen_width / glyph_width) * glyph_width < screen_width ||
+           (screen_height / glyph_height) * glyph_height < screen_height {
+            screen_width = (screen_width / glyph_width) * glyph_width;
+            screen_height = (screen_height / glyph_height) * glyph_height;
             try!(renderer.window_mut().unwrap().set_size(screen_width, screen_height));
         }
-        
-        for x in 0..screen_width/glyph_width {
-            for y in 0..screen_height/glyph_height {
-                let target = Rect::new((x * glyph_width) as i32, (y * glyph_height) as i32, glyph_width, glyph_height);
+
+        for x in 0..screen_width / glyph_width {
+            for y in 0..screen_height / glyph_height {
+                let target = Rect::new((x * glyph_width) as i32,
+                                       (y * glyph_height) as i32,
+                                       glyph_width,
+                                       glyph_height);
                 renderer.copy(&mut dot, None, Some(target));
             }
         }
-        renderer.present();            
-        
+        renderer.present();
+
         thread::sleep(Duration::from_millis(10));
         i += 1;
         if i > 9 {
             i = 0;
         }
     }
-    
+
     Ok(())
 }
 
 fn render_text(renderer: &mut Renderer, font: &Font, i: i32) -> Result<Texture, ScapeError> {
-    let surface = try!(font.render(format!("{}", i).as_str()).blended(Color::RGBA(255, 0, 0, 255)).map_err(|e| e.to_string())); //more private error types :(
+    let surface = try!(font.render(format!("{}", i).as_str())
+                           .blended(Color::RGBA(255, 0, 0, 255))
+                           .map_err(|e| e.to_string())); //more private error types :(
     let texture = try!(renderer.create_texture_from_surface(&surface));
-    
+
     Ok(texture)
 }
